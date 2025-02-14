1b551474d0e51feb5b039ef9ea5a83fcba17170a
Smilez/Get-Exploits <smilezrealscripts@gmail.com>
2025-02-14 13:47:49 -0500

    Update Unc-Sunc-Vuln-Test.lua

--- Unc-Sunc-Vuln-Test.lua
+++ Unc-Sunc-Vuln-Test.lua
@@ -1,3 +1,30 @@
+local function callback(Text)
+end
+
+local NotificationBindable = Instance.new("BindableFunction")
+NotificationBindable.OnInvoke = callback
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Thanks For Using Smilez Unc, Sunc, And Vuln Checker.";
+    Text = "Loading...";
+    Duration = "5";
+    Callback = NotificationBindable;
+})﻿
+wait(5)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "SUSVC.";
+    Text = "Loading Complete.";
+    Duration = "5";
+    Callback = NotificationBindable;
+})﻿
+wait(2)
+game.StarterGui:SetCore("SendNotification", {
+    Title = "SUSVC.";
+    Text = "Press F9 To Check The Console. Please Be Patient As Some Tests Can Take Up To 60 Seconds To Complete Depending On Your Internet Connection.";
+    Duration = "5";
+    Callback = NotificationBindable;
+})﻿
+
 print("unc test below")
 wait(1)
 print("unc test below")
@@ -37,4 +64,4 @@ wait(1)
 wait(5)
 loadstring(game:HttpGet("https://github.com/SmilezReal/Executor-Tests/raw/refs/heads/main/VulnTest.lua",true))()
 wait(1)
-print("if any of these test didnt work. please re-execute the script. if the issues still persist. your executor does not support this script.")
+print("if any of these tests didnt work. please re-execute the script. if the issues still persist. your executor does not support this script.")
